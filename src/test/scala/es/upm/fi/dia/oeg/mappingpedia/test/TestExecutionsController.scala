6effe5e739aeee2c4bea4b4abc9dd128f7c9bcc9
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-10-15 12:20:48 +0200

    update test

--- src/test/scala/es/upm/fi/dia/oeg/mappingpedia/test/TestExecutionsController.scala
+++ src/test/scala/es/upm/fi/dia/oeg/mappingpedia/test/TestExecutionsController.scala
@@ -9,10 +9,11 @@ object TestExecutionsController {
   val executionsController = MappingExecutionController.apply();
 
   def main(args:Array[String]) = {
-    //this.testExecuteMapping1();
-    this.testExecuteMapping2();
+    this.testExecuteMapping1();
+    //this.testExecuteMapping2();
     //this.testQueryMapping();
     //this.testFindInstancesByClassName("Building");
+    //this.testQueryMapping2();
   }
 
   def testExecuteMapping1() {
@@ -45,15 +46,28 @@ object TestExecutionsController {
     logger.info(s"resultDownloadUrl = ${resultDownloadUrl}")
   }
 
+
   def testFindInstancesByClassName(className:String) = {
     val instances = executionsController.findInstancesByClass(className)
     logger.info(s"instances = ${instances}")
   }
 
-  def testQueryMapping() {
+  def testQueryMapping1() {
     val datasetId = "5f0ac042-a289-4a5a-a1ad-a0a760cfbcf9"
     val mdId= "0cc9daaa-b425-40e9-a74c-e5ea88a17cc1";
     val queryUrl = "https://raw.githubusercontent.com/oeg-upm/morph-rdb/master/morph-examples/examples-csv/edificio-historico-q1.rq";
+
+    val mappingExecution = MappingExecution(datasetId, mdId, queryUrl);
+
+    val result = executionsController.executeMapping(mappingExecution);
+    val resultDownloadUrl = result.getMapping_execution_result_download_url;
+    logger.info(s"resultDownloadUrl = ${resultDownloadUrl}")
+  }
+
+  def testQueryMapping2() {
+    val datasetId = "f0eb922b-122b-49e9-a4d2-fa0eb8b592d6"
+    val mdId= "300bea7f-0a5d-4962-bb77-64193c571d49";
+    val queryUrl = "https://raw.githubusercontent.com/oeg-upm/morph-rdb/master/morph-examples/examples-csv/preview.rq";
 
     val mappingExecution = MappingExecution(datasetId, mdId, queryUrl);
 
